d1fa7d15146e96b4cc9ab3005e490d83d16ba445
Rasmus Larsson <dat11rla@student.lu.se>
2013-01-28 10:00:18 +0100

    Added testEmpty and testOne to resultwriter test

--- src/test/TestResultWriter.java
+++ src/test/TestResultWriter.java
@@ -5,7 +5,10 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,6 +28,7 @@ public class TestResultWriter {
 	private Map<Integer, Racer> map;
 	private ResultWriter writer;
 	private String filename;
+	private String header;
 	
 	/**
 	 * @throws java.lang.Exception
@@ -32,6 +36,7 @@ public class TestResultWriter {
 	@Before
 	public void setUp() throws Exception {
 		filename = "test.txt";
+		header = "StartNr; Totaltid; Starttid; Måltid\n";
 		map = new HashMap<Integer, Racer>();
 		writer = new ResultWriter(map, filename);
 		
@@ -54,9 +59,41 @@ public class TestResultWriter {
 		file.delete();
 	}
 
+	/**
+	 * Reads the testfile and returns it contents
+	 * @return The testfiles contents
+	 * @throws IOException
+	 */
+	private String readFile() throws IOException {
+		FileReader file = new FileReader(filename);
+		BufferedReader reader = new BufferedReader(file);
+		
+		StringBuilder sb = new StringBuilder();
+		String line;
+		
+		while ((line = reader.readLine()) != null) {
+			sb.append(line);
+			sb.append("\n");
+		}
+		
+		return sb.toString();
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testOneRacer() throws IOException {
+		Racer racer = new Racer(1);
+		
+		map.put(racer.getStartNumber(), racer);
+		writer.writeToFile();
+		
+		assertEquals("Result doesn't match", header + racer.toString() + "\n", readFile());
+	}
+
+	@Test
+	public void testEmpty() throws IOException {
+		writer.writeToFile();
+		
+		assertEquals("Result not empty", header, readFile());
 	}
 
 }
