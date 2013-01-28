dab0d652534e3c5626349e5446b3f79249da7544
Rasmus Larsson <dat11rla@student.lu.se>
2013-01-28 09:28:20 +0100

    Added testcase skeleton for ResultWriter

--- src/registration/ResultWriter.java
+++ src/registration/ResultWriter.java
@@ -1,5 +1,22 @@
 package registration;
 
-public class ResultWriter {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
 
+import racer.Racer;
+
+public class ResultWriter {
+	private Map<Integer, Racer> data;
+	private String filename;
+	
+	public ResultWriter(Map data, String filename) {
+		this.data = data;
+		this.filename = filename;
+	}
+	
+	public void writeToFile() {
+		
+	}
 }
