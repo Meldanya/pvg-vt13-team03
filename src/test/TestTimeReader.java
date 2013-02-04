cfa3ad3695b1326d7c9816594149ead9761fd6fa
Rasmus Larsson <dat11rla@student.lu.se>
2013-02-04 16:32:13 +0100

    Moved unit test testing files

--- src/test/TestTimeReader.java
+++ src/test/TestTimeReader.java
@@ -46,7 +46,7 @@ public class TestTimeReader {
 		tempMap.get("3").add("1359359447");
 		tempMap.put("4", new ArrayList<String>());
 		tempMap.get("4").add("1359359448");
-		Map<String, ArrayList<String>> map = reader.readFromTimeFile("src/test/teststart.txt");
+		Map<String, ArrayList<String>> map = reader.readFromTimeFile("test/TestTimeReader/start.txt");
 
 		for(String s : map.keySet()){
 			assertEquals(tempMap.get(s).size(), map.get(s).size());
@@ -56,7 +56,7 @@ public class TestTimeReader {
 	
 	@Test
 	public void testIncorrectlyFormattedFile(){
-		assertEquals(reader.readFromTimeFile("src/test/incorrectstart.txt"), null);
+		assertEquals(reader.readFromTimeFile("test/TestTimeReader/incorrectstart.txt"), null);
 	}
 
 }
