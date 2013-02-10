e5122d928676dc0737e79b42ebd2243569e6911d
Alexander Johansson Werne <dat11ajo@student.lu.se>
2013-02-10 15:20:18 +0100

    Removed dependency of Racer class in Register classes

--- src/registration/FinishRegister.java
+++ src/registration/FinishRegister.java
@@ -1,20 +1,8 @@
 package registration;
 
-import racer.RacerTime;
 import res.Strings;
 
 public class FinishRegister extends Register {
-
-	@Override
-	protected String getRacerTime() {
-		return racer.getFinishTime();
-	}
-
-	@Override
-	protected void addTime(RacerTime racerTime) {
-		racer.addFinishTime(racerTime);
-		
-	}
 
 	@Override
 	protected String getFileName() {
