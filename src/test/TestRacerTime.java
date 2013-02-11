721e5277e06a903f88b4bc8c469dc6b2a117fa96
Daniel Olsson <atf10dol@student.lu.se>
2013-02-11 10:03:48 +0100

    Gjort RacerTime Comparable

--- src/test/TestRacerTime.java
+++ src/test/TestRacerTime.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -30,5 +31,23 @@ public class TestRacerTime {
 	public void testComplexCompareTo() {
 		RacerTime compare = new RacerTime("22.13.14");
 		assertEquals("Diff not correct", "11.01.01", racerTime.getDifferenceTo(compare));
+	}
+	
+	@Test
+	public void testEquals(){
+		RacerTime time1 = new RacerTime("11.11.11");
+		RacerTime time2 = new RacerTime("11.11.11");
+		
+		assertEquals("RacerTimes are not equal", time1, time2);
+		assertEquals("RacerTimes are not equal", 0, time1.compareTo(time2));
+	}
+	
+	@Test
+	public void testComparable() {
+		RacerTime time1 = new RacerTime("11.11.11");
+		RacerTime time2 = new RacerTime("11.13.11");
+		
+		assertTrue("racerTime not greater than time1", racerTime.compareTo(time1) > 0);
+		assertTrue("racerTime not less than time2", racerTime.compareTo(time2) < 0);
 	}
 }
