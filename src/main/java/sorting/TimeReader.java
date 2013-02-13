a6d3cda9d58164040e3f2c7c9036424d1ff20bd3
Alexander Johansson Werne <dat11ajo@student.lu.se>
2013-02-13 16:50:28 +0100

    Made TimeReader more generic

--- src/main/java/sorting/TimeReader.java
+++ src/main/java/sorting/TimeReader.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TimeReader extends Reader {
-	private HashMap<String, ArrayList<String>> map;
+	private Map<String, ArrayList<String>> map;
 
 	protected void op(String key, String value) {
 		if(!map.containsKey(key)){
