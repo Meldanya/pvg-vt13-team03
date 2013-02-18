1ddaebfa7024613b809a372e93257f870ede09fe
Alexander Johansson Werne <dat11ajo@student.lu.se>
2013-02-18 11:48:50 +0100

    Fixed imports

--- src/main/java/sorting/Sorter.java
+++ src/main/java/sorting/Sorter.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import racer.RacerPlacingComparator;
