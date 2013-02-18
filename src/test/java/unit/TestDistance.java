187e9da58d853de00ffcb6c0bc1fc77bfda67910
Gustaf Ekenstein <dat11ge1@student.lu.se>
2013-02-18 15:45:50 +0100

    Created test for distance

---
+++ src/test/java/unit/TestDistance.java
@@ -0,0 +1,27 @@
+package unit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import racer.Distance;
+
+public class TestDistance {
+
+	private Distance distance;
+
+	@Before
+	public void setUp() throws Exception {
+		distance = new Distance();
+	}
+	@Test
+	public void testTimeFormatting() {
+		
+		distance.setStartTime("11.00.00");
+		distance.setFinishTime("12.00.00");
+		
+		assertEquals("Formatting is wrong", "01.00.00; 11.00.00; 12.00.00", distance.toString());
+	}
+
+}
