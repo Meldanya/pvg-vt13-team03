3abea8ebd61c284138439994e47fa5647b2e94b3
Alexander Johansson Werne <alex_werne@hotmail.com>
2013-02-26 19:52:41 +0100

    Updated the test.

--- src/test/java/unit/TestMarathonRacer.java
+++ src/test/java/unit/TestMarathonRacer.java
@@ -11,30 +11,35 @@ import racer.MarathonRacer;
 
 public class TestMarathonRacer {
 
-	private AbstractRacer marathonRacer;
+	private AbstractRacer marathonRacer1, marathonRacer2;
 
 	@Before
 	public void setUp() throws Exception {
-		marathonRacer = new MarathonRacer("1");
-		marathonRacer.setName("Anders Asson");
-		marathonRacer.addStartTime("12.00.00");
-		marathonRacer.addFinishTime("13.23.34");
+		marathonRacer1 = new MarathonRacer("1");
+		marathonRacer1.setName("Anders Asson");
+		marathonRacer1.addStartTime("12.00.00");
+		marathonRacer1.addFinishTime("13.23.34");
+		
+		marathonRacer2 = new MarathonRacer("2");
+		marathonRacer2.setName("Erik Jansson");
+		marathonRacer2.addStartTime("00.00.00");
+		marathonRacer2.addFinishTime("13.23.34");
 	}
 
 	@Test
 	public void testTimeFormatting() {
-		assertEquals("Formatting is wrong", "1; Anders Asson; 01.23.34; 12.00.00; 13.23.34", marathonRacer.toString());
+		assertEquals("Formatting is wrong", "1; Anders Asson; 01.23.34; 12.00.00; 13.23.34", marathonRacer1.toString());
 	}
 	
 	 @Test
 	 public void testGetTotalTime(){
 		 //TODO getTotalTime should probably not include a semicolon.
-		 assertEquals("Total time is incorrect", "; 01.23.34",marathonRacer.getTotalTime());
+		 assertEquals("Total time is incorrect for racer 1", "; 01.23.34",marathonRacer1.getTotalTime());
+		 assertEquals("Total time is incorrect for racer 2", "; 13.23.34", marathonRacer2.getTotalTime());
 	 }
 	
 	@After
 	public void tearDown(){
-		marathonRacer = null;
+		marathonRacer1 = null;
 	}
 }
-
