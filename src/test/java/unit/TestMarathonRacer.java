0a169f6d1b90b7dad01341017c1a1fb211050f66
Alexander Johansson Werne <alex_werne@hotmail.com>
2013-02-26 22:29:19 +0100

    Ignored test for unimplemented functionality.

--- src/test/java/unit/TestMarathonRacer.java
+++ src/test/java/unit/TestMarathonRacer.java
@@ -21,9 +21,9 @@ public class TestMarathonRacer {
 		marathonRacer1.addFinishTime("13.23.34");
 		
 		marathonRacer2 = new MarathonRacer("2");
-		marathonRacer2.setName("Erik Jansson");
-		marathonRacer2.addStartTime("00.00.00");
-		marathonRacer2.addFinishTime("13.23.34");
+		marathonRacer2.setName("Bengt Bsson");
+		marathonRacer2.addStartTime("12.01.00");
+		marathonRacer2.addFinishTime("13.15.16");
 	}
 
 	@Test
@@ -35,7 +35,7 @@ public class TestMarathonRacer {
 	 public void testGetTotalTime(){
 		 //TODO getTotalTime should probably not include a semicolon.
 		 assertEquals("Total time is incorrect for racer 1", "; 01.23.34",marathonRacer1.getTotalTime());
-		 assertEquals("Total time is incorrect for racer 2", "; 13.23.34", marathonRacer2.getTotalTime());
+		 assertEquals("Total time is incorrect for racer 2", "; 01.14.16", marathonRacer2.getTotalTime());
 	 }
 	
 	@After
