52e1836281bff6175c51b73e3e9883609df02623
Alexander Johansson Werne <alex_werne@hotmail.com>
2013-02-26 21:33:13 +0100

    Renamed getStartTime

--- src/main/java/racer/MarathonRacer.java
+++ src/main/java/racer/MarathonRacer.java
@@ -6,7 +6,7 @@ public class MarathonRacer extends AbstractRacer {
 		
 	}
 
-	public String getStartTime() {
+	public String startTimeString() {
 		return firstDistance().getStartTime().toString();
 	}
 
