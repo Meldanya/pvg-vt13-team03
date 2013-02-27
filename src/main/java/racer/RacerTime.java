03b1632e2d65687b2cecb5e401100f6765534a08
Alexander Johansson Werne <dat11ajo@student.lu.se>
2013-02-27 12:45:18 +0100

    Made format(RacerTime) now return '--.--.--' if duration is 0

--- src/main/java/racer/RacerTime.java
+++ src/main/java/racer/RacerTime.java
@@ -39,36 +39,40 @@ public class RacerTime implements Comparable<RacerTime> {
 	}
 
 	public static String format(long duration) {
-		StringBuilder sb = new StringBuilder();
+		if (duration == 0) {
+			return "--.--.--";
+		} else {
+			StringBuilder sb = new StringBuilder();
 
-		long hours = TimeUnit.MILLISECONDS.toHours(duration);
-		duration -= hours * 60 * 60 * 1000;
-		if (hours < 10) {
-			sb.append('0');
+			long hours = TimeUnit.MILLISECONDS.toHours(duration);
+			duration -= hours * 60 * 60 * 1000;
+			if (hours < 10) {
+				sb.append('0');
+			}
+			sb.append(Long.toString(hours));
+
+			sb.append('.');
+
+			long minutes = TimeUnit.MILLISECONDS.toMinutes(duration);
+			duration -= minutes * 60 * 1000;
+			if (minutes < 10) {
+				sb.append('0');
+			}
+			sb.append(Long.toString(minutes));
+
+			sb.append('.');
+
+			long seconds = TimeUnit.MILLISECONDS.toSeconds(duration);
+			if (seconds < 10) {
+				sb.append("0");
+			}
+			sb.append(Long.toString(seconds));
+
+			return sb.toString();
 		}
-		sb.append(Long.toString(hours));
-
-		sb.append('.');
-
-		long minutes = TimeUnit.MILLISECONDS.toMinutes(duration);
-		duration -= minutes * 60 * 1000;
-		if (minutes < 10) {
-			sb.append('0');
-		}
-		sb.append(Long.toString(minutes));
-
-		sb.append('.');
-
-		long seconds = TimeUnit.MILLISECONDS.toSeconds(duration);
-		if (seconds < 10) {
-			sb.append("0");
-		}
-		sb.append(Long.toString(seconds));
-
-		return sb.toString();
 	}
 
-	public long computeLapTime(RacerTime other){
+	public long computeLapTime(RacerTime other) {
 		return other.time.getTime() - time.getTime();
 	}
 
@@ -101,8 +105,8 @@ public class RacerTime implements Comparable<RacerTime> {
 			return false;
 		return true;
 	}
-	
-	public long getTime(){
+
+	public long getTime() {
 		return time.getTime();
 	}
 }
